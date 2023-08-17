b1042457d8d7a2af741ebf1657197f030ae06def
Aline Grance <grance.aline@gmail.com>
2023-08-17 15:00:55 -0300

    Do clean and refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # every route is here now
+  resources :articles # resources provides REST-ful routes to Rails resources
+  # REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
 end
