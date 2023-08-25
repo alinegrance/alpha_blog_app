d2294c20fe4a46f6fdbb60b083418b0111cb6266
Aline Grance <grance.aline@gmail.com>
2023-08-25 14:34:39 -0300

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # every route is here now
   resources :articles # resources provides REST-ful routes to Rails resources
   # REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
+
+
