797cd3baf81b7a96a61b49adeee98f3f41b4dd1f
Aline Grance <grance.aline@gmail.com>
2023-08-31 15:20:22 -0300

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  # sessions wont have full crud so wont have a 'resources'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
 
 
