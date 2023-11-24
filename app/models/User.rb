9ca94e1ae76d01c49d7bdf988374879071c4f317
Aline Grance <grance.aline@gmail.com>
2023-11-24 14:17:04 -0300

    Add admin to users table

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :articles, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  validates :username, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 25}
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 105}, format: {with: URI::MailTo::EMAIL_REGEXP }
-
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: { maximum: 105},
+            format: {with: URI::MailTo::EMAIL_REGEXP }
 
   has_secure_password
 end
