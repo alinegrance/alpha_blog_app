85f45fbb0325c81212a442aa16b3882a1e4e4f30
Aline Grance <grance.aline@gmail.com>
2023-11-29 11:39:35 -0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!([{
+                username: "blog_admin",
+                email: "blog_admin@email.com",
+                password: "blog_admin123",
+                admin: true,
+              }])
